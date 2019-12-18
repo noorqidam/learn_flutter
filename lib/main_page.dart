aa8afdc73c7c1d3b65d41a03e8659544d598af70
noorqidam <noorqidam@gmail.com>
2019-12-18 13:20:24 +0700

    Navigasi Page Screen

---
+++ lib/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:first_app/second_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go to Second Page"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}

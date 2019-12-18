ffec3c4f17c0fbfe71629c43b879391e6ce75bd3
noorqidam <noorqidam@gmail.com>
2019-12-18 16:06:40 +0700

    Card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(
-            Icons.adb,
-            color: Colors.white,
-          ),
-          title: Text(
-            "AppBar Example",
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.exit_to_app),
-              onPressed: () {},
-            )
-          ],
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Color(0xff0096ff), Color(0xff6610f2)],
-                    begin: FractionalOffset.topLeft,
-                    end: FractionalOffset.bottomRight),
-                image: DecorationImage(
-                    image: AssetImage("assets/pattern.png"),
-                    fit: BoxFit.none,
-                    repeat: ImageRepeat.repeat)),
+        backgroundColor: Colors.green,
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              buildCard(Icons.account_box, "Account Box"),
+              buildCard(Icons.adb, "Serangga Android"),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: <Widget>[
+          Container(margin: EdgeInsets.all(5)),
+          Icon(iconData),
+          Text(text)
+        ],
       ),
     );
   }

e3c6d7bbc409adf3a6a4fbd3650bfd9a9c4e2f8a
noorqidam <noorqidam@gmail.com>
2019-12-19 15:35:43 +0700

    AppBar Custom Height

--- lib/main.dart
+++ lib/main.dart
@@ -7,73 +7,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlue[50],
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Latihan Hero Animation",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return SecondPage();
-          }));
-        },
-        child: Hero(
-          tag: 'pp',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: Container(
-              width: 100,
-              height: 100,
-              child: Image(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    "https://inikpop.com/wp-content/uploads/2018/12/jennie.jpg"),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink[50],
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Latihan Hero Animation",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Center(
-        child: Hero(
-          tag: 'pp',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Container(
-              width: 200,
-              height: 200,
-              child: Image(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    "https://inikpop.com/wp-content/uploads/2018/12/jennie.jpg"),
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: AppBar(
+            backgroundColor: Colors.amber,
+            flexibleSpace: Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  "Appbar with Custom Height",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
               ),
             ),
           ),

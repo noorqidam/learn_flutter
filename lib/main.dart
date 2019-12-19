20d5ee1dd7f82751269370ffb1b643cc0e44313e
noorqidam <noorqidam@gmail.com>
2019-12-19 17:56:35 +0700

    Menampilkan QR Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TabBar myTabBar = TabBar(
-      // indicatorColor: Colors.red,
-      indicator: BoxDecoration(
-          color: Colors.red,
-          border: Border(top: BorderSide(color: Colors.purple, width: 5))),
-      tabs: <Widget>[
-        Tab(
-          icon: Icon(Icons.comment),
-          text: "Comments",
-        ),
-        Tab(
-          icon: Icon(Icons.computer),
-          text: "Computers",
-        )
-      ],
-    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-              title: Text("Contoh Tab Bar"),
-              bottom: PreferredSize(
-                preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-                child: Container(
-                  color: Colors.amber,
-                  child: myTabBar,
-                ),
-              )),
-          body: TabBarView(
-            children: <Widget>[
-              Center(
-                child: Text("Tab 1"),
-              ),
-              Center(
-                child: Text("Tab 2"),
-              ),
-            ],
+      home: Scaffold(
+        body: Center(
+          child: QrImage(
+            version: 6,
+            backgroundColor: Colors.grey,
+            foregroundColor: Colors.black,
+            errorCorrectionLevel: QrErrorCorrectLevel.M,
+            padding: EdgeInsets.all(30),
+            size: 300,
+            data: "https://www.youtube.com/channel/UC0s92eMIsyR9otmIiY4v8Ww",
           ),
         ),
       ),

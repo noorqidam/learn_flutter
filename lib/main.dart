0beec2467552ad15d60a1dbd767cbf8f27c4a049
noorqidam <noorqidam@gmail.com>
2019-12-19 17:22:46 +0700

    Mengubah Tambilan TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,33 +5,36 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = TabBar(
+      // indicatorColor: Colors.red,
+      indicator: BoxDecoration(
+          color: Colors.red,
+          border: Border(top: BorderSide(color: Colors.purple, width: 5))),
+      tabs: <Widget>[
+        Tab(
+          icon: Icon(Icons.comment),
+          text: "Comments",
+        ),
+        Tab(
+          icon: Icon(Icons.computer),
+          text: "Computers",
+        )
+      ],
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
-        length: 4,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
-            title: Text("Contoh Tab Bar"),
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.comment),
-                  text: "Comments",
+              title: Text("Contoh Tab Bar"),
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+                child: Container(
+                  color: Colors.amber,
+                  child: myTabBar,
                 ),
-                Tab(
-                  child: Image(
-                    image: AssetImage("assets/images/cute_icon.png"),
-                  ),
-                ),
-                Tab(
-                  icon: Icon(Icons.computer),
-                ),
-                Tab(
-                  text: "News",
-                )
-              ],
-            ),
-          ),
+              )),
           body: TabBarView(
             children: <Widget>[
               Center(
@@ -40,12 +43,6 @@ class MyApp extends StatelessWidget {
               Center(
                 child: Text("Tab 2"),
               ),
-              Center(
-                child: Text("Tab 3"),
-              ),
-              Center(
-                child: Text("Tab 4"),
-              )
             ],
           ),
         ),
